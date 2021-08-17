d79511108a7a9bfe94de3ad600cd202ec3f97700
Emilio P <ejpascualj@gmail.com>
2021-08-16 21:14:06 -0500

    Pushing to Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sample_db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sample', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
